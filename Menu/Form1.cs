763c28d747b92e66ff983610615689f7b5df133b
angela chaj <angelachaj648@gmail.com>
2026-05-16 16:12:17 -0600

    inicia partida

--- Menu/Form1.cs
+++ Menu/Form1.cs
@@ -6,3 +6,26 @@ namespace Menu
 		public void MostrarMenu()
 		{
 			bool salir = false;
+			while (!salir)
+			{
+				Console.Clear();
+
+
+				Console.WriteLine("-----------------------------------");
+				Console.WriteLine("       JUEGO DE ESTRATEGIAS");
+				Console.WriteLine("------------------------------------");
+
+				
+
+				Console.WriteLine("1. Iniciar Partida");
+				Console.WriteLine("2. Ver Reglas");
+				Console.WriteLine("3. Ver Puntajes");
+				Console.WriteLine("4. Salir");
+
+				Console.WriteLine();
+				Console.Write("Seleccione una opción: ");
+
+				string opcion =
+					Console.ReadLine();
+				switch (opcion)
+				{ 
