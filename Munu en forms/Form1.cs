e313b14763a38945208bab9987443f25650765df
angela chaj <angelachaj648@gmail.com>
2026-05-16 22:16:32 -0600

    las reglas del juego

--- Munu en forms/Form1.cs
+++ Munu en forms/Form1.cs
@@ -10,5 +10,26 @@ namespace Juego_de_Estrategias
 		{
 			InitializeComponent();
 		}
+		private void iniciarToolStripMenuItem_Click(
+			object sender,
+			EventArgs e)
+		{
+			Form1 login =
+				new Form1();
+
+			login.Show();
+
+			Hide();
+		}
+
+		private void reglasToolStripMenuItem_Click(object sender,
+			
+			EventArgs e)
+		{
+			MessageBox.Show(
+				"REY: una casilla" +
+				"TORRE: horizontal y vertical" +
+				"SOLDADO: avanza y captura diagonal");
+		}
 
 		
