aa9579159bd14df7f5de01c472888961ee0c6612
angela chaj <angelachaj648@gmail.com>
2026-05-16 22:14:54 -0600

    metodo del menu

---
+++ Munu en forms/Form1.cs
@@ -0,0 +1,14 @@
+using Munu_en_forms;
+using System;
+using System.Windows.Forms;
+
+namespace Juego_de_Estrategias
+{
+	public partial class FormMenu : Form
+	{
+		public FormMenu()
+		{
+			InitializeComponent();
+		}
+
+		
